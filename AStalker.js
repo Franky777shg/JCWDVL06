4180503cb21bf41c867b1e665950c2e438b908e5
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-05-11 21:59:21 +0700

    logic statement

--- AStalker.js
+++
@@ -1,5 +0,0 @@
-let word = "atlanticA";
-
-let result = word.replace(/a/g, "a.").replace(/A/g, "A.");
-
-console.log(result); // "a.tla.nticA."
---
+++ Review.js
@@ -0,0 +1,3 @@
+// if else vs switch case
+// tidak ada penggunaan if else atau switch case yang harus digunakan dalam case tertentu
+// perbedaannya hanya di syntax dan preferensi coder (untuk sekarang)
