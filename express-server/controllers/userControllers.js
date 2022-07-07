387fc54c6bd223d6c3a2558edfe9fff6eab1fd6f
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-07-07 20:03:55 +0700

    api finished

--- express-server/controllers/userControllers.js
+++ express-server/controllers/userControllers.js
@@ -32,8 +32,41 @@ module.exports = {
       res.status(200).send(result);
     });
   },
-  // register
-  // edit
-  // delete
-  // buat fitur login, register, dan keep login dari front end menggunakan api yang kita buat sendiri
+  register: (req, res) => {
+    const { username, password, email } = req.body;
+    let addQuery = `insert into users(username, password, email) values (${db.escape(
+      username
+    )}, ${db.escape(password)}, ${db.escape(email)})`;
+
+    db.query(addQuery, (err, result) => {
+      if (err) return console.log(err);
+
+      res.status(200).send(result);
+    });
+  },
+  edit: (req, res) => {
+    let arr = [];
+    for (let key in req.body) {
+      arr.push(`${key} = ${db.escape(req.body[key])}`);
+    }
+
+    let updateQuery = `update users set ${arr} where idusers = ${+req.params
+      .id}`;
+
+    db.query(updateQuery, (err, result) => {
+      if (err) return console.log(err);
+
+      res.status(200).send(result);
+    });
+  },
+  delete: (req, res) => {
+    let deleteQuery = `delete from users where idusers = ${+req.params.id}`;
+
+    db.query(deleteQuery, (err, result) => {
+      if (err) return console.log(err);
+
+      res.status(200).send(result);
+    });
+  },
+  // buat fitur login dan register dari front end menggunakan api yang kita buat sendiri
 };
--- express-server/routers/userRouters.js
+++ express-server/routers/userRouters.js
@@ -5,5 +5,8 @@ const { user } = require("../controllers");
 router.get("/getAll", user.getAllUsers);
 router.get("/getById/:id", user.getUserById);
 router.post("/login", user.login);
+router.post("/register", user.register);
+router.post("/edit/:id", user.edit);
+router.delete("/delete/:id", user.delete);
 
 module.exports = router;
